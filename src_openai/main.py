e301891dd620c8bdf7888da240b8a8ddf5a55536
Ghost <OverlyGenericAddress@pm.me>
2023-09-03 15:45:50 +0200

    Implemented openAI API

--- src_openai/main.py
+++ src_openai/main.py
@@ -1,7 +1,7 @@
 import os
-import guidance
 from dotenv import load_dotenv, find_dotenv
 from flask import Flask, request, jsonify
+from src_openai.pipeline.custom_prompter import model_generate
 
 # Create the flask app
 app = Flask(__name__)
@@ -9,70 +9,35 @@ app = Flask(__name__)
 
 # Listen for POST requests to /generate
 @app.route('/generate', methods=['POST'])
-def generate():
+def api_generate():
     # Check the API key
-    if request.headers.get('X-API-Key') != backend_key:
+    if request.headers.get('BeKey') != backend_key:
         return jsonify({'message': 'Invalid API key'}), 403
     else:
         # Get the input data from the request
         data = request.get_json()
 
+        print(data)
+
         # Generate the response
-        response = generate(prompt=data['prompt'],
-                            systempromt=data['systemPrompt'],
-                            relationship=data['relationship'],
-                            temprature=data['settings']['temperature'],
-                            max_tokens=data['settings']['maxTokens'])
-        
+        generated_response = model_generate(
+            prompt=data['userPrompt'],
+            system_prompt=data['systemPrompt'],
+            relationship=data['systemRelationship'],
+            model_name=data['modelName'],
+            temperature=data['temperature'],
+            max_tokens=data['maxTokens'],
+            openai_key=openai_key)
 
         # Return the results
-        return jsonify({'response': response})
+        return jsonify({'answer': generated_response})
 
-
-# Use the guidance program to generate a response
-def generate(prompt str,
-             systempromt str='You are a helpful digital assistant.',
-             relationship str='This is the first time the user meets with his new assistant.',
-             model str='gpt-3.5-turbo',
-             temprature float=0.5,
-             max_tokens int=256) -> str:
-    
-
-
-    # Define a guidance template for generating the response
-    generated_template = guidance("""                 
-    {{#system~}}
-    {{systempromt}}
-    {{~/system}}
-
-    {{#user~}}
-    Here are some information about the user:
-    {{prompt}}
-    {{~/user}}
-
-    {{#assistant~}}
-    Here are some information about the users assistant and their relationship:
-    {{relationship}}
-    {{~/assistant}}
-                                  
-    {{#assistant~}}
-    {{gen 'response' {{temperature}} {{max_tokens}}}}
-    {{~/assistant}}
-    """, prompt=prompt, systempromt=systempromt, character_relationship=character_relationship, temperature=temprature, max_tokens=max_tokens, model=model)
-    
-    # Generate the response
-    try:
-        generated_text = generated_template()
-    except Exception as e:
-        generated_text = str(e)
-
-    # Return the result
-    return generated_text
 
 if __name__ == '__main__':
     # Load the environment variables
     load_dotenv(find_dotenv())
     backend_key = os.getenv('BACKEND_API_KEY')
+    openai_key = os.getenv('OPENAI_API_KEY')
 
     # Start the app
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='localhost', port=5005)
---
+++ src_openai/pipeline/custom_prompter.py
@@ -0,0 +1,47 @@
+import guidance
+
+
+# Use the guidance program to generate a response
+def model_generate(
+        prompt: str,
+        system_prompt: str,
+        relationship: str,
+        model_name: str,
+        temperature: float,
+        max_tokens: int,
+        openai_key: str, ) -> str:
+    # Set the model used for generating the response
+    llm_model = guidance.llms.OpenAI(model=model_name, api_key=openai_key)
+
+    # Define a guidance template for generation
+    generated_template = llm_model('''                
+    {{#system~}}
+    {{system_prompt}}
+    {{~/system}}
+
+    {{#user~}}
+    Here are some information about the user:
+    {{prompt}}
+    {{~/user}}
+
+    {{#assistant~}}
+    Here are some information about the users assistant and their relationship:
+    {{relationship}}
+    {{~/assistant}}
+
+    {{#assistant~}}
+    {{gen 'response' {{temperature}} {{max_tokens}}}}
+    {{~/assistant}}
+    ''')
+
+    # Try to generate the response using the guidance template with the openai model
+    try:
+        # Generate the response
+        generated_text = generated_template(
+            system_prompt, prompt, relationship, temperature, max_tokens)
+    # If an error occurs, return the error message instead
+    except Exception as e:
+        generated_text = str(e)
+
+    # Return the result of the generation attempt
+    return generated_text
