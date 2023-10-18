ac3b773575a5449b31fb5c15c76f78cefe6d7212
Ghost <OverlyGenericAddress@pm.me>
2023-10-18 17:28:38 +0200

    Fixed issue where server wouldn't start

--- src/main.py
+++ src/main.py
@@ -10,29 +10,29 @@ Functions:
 """
 from dotenv import load_dotenv
 from fastapi import FastAPI
-from src.package import package
-from src.model import api_replicate as model
-
-if __name__ == '__main__':
-    # Load the environment variables
-    load_dotenv()
-
-    # Initialize the app
-    app = FastAPI()
+from package import package
+from model import api_replicate as model
 
 
-    @app.post("/api/generate")
-    async def generate(generate_json: dict) -> dict:
-        """
-        This function handles the /api/generate endpoint.
+# Load the environment variables
+load_dotenv()
 
-        :param generate_json: The application wide generate format.
-        :type generate_json: generate.json
-        :return: The application wide content format.
-        :rtype: content.json
-        """
-        try:
-            generated_content = model.chat_llama2_13b(prompt=generate_json['prompt'])
-            return package.content_text(full_text_content=generated_content)
-        except Exception as e:
-            return package.content_text(error_occurred=str(e))
+# Initialize the app
+app = FastAPI()
+
+
+@app.post("/api/generate")
+async def generate(generate_json: dict) -> dict:
+    """
+    This function handles the /api/generate endpoint.
+
+    :param generate_json: The application wide generate format.
+    :type generate_json: generate.json
+    :return: The application wide content format.
+    :rtype: content.json
+    """
+    try:
+        generated_content = model.chat_llama2_13b(prompt=generate_json['prompt'])
+        return package.content_text(full_text_content=generated_content)
+    except Exception as e:
+        return package.content_text(error_occurred=str(e))
